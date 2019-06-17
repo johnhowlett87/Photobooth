02499c19490744b2c003dee609d189d8d8dfced1
johnhowlett87 <46422234+johnhowlett87@users.noreply.github.com>
2019-06-18 00:01:18 +0100

    Version 1: Working on laptop
    Next step to port it to raspberry pi. Also som rigurous testing.  Maybe some enhancements to allow for better identification of the next image

--- photoboothv2.py
+++ photoboothv2.py
@@ -28,6 +28,11 @@ from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QGroupBox, QHBox
     QTableWidgetItem, QStyleFactory, QListWidgetItem, QScrollArea
 
 
+SITE_BASE='http://ezshare.card/'
+DIR='./static/images/'
+CAM='106PANA'
+INTERVAL=500
+
 # This is the class for images. We'll to storing these in a photobooth object.
 # each image object will have methods to call the full path, current view count and image name (for the dict key)
 # We'll also need to increment the view count.
@@ -38,15 +43,6 @@ class image:
         self.name = name
         self.path = path
         self.view_count = view_count
-
-    def getName(self):
-        return self.name
-
-    def getPath(self):
-        return self.path
-
-    def getViewCount(self):
-        return self.view_count
 
     def incrementViewCount(self):
         self.view_count = self.view_count + 1
@@ -75,14 +71,17 @@ class App(QWidget):
         self.setLayout(self.layout)
 
         self.qTimer = QTimer()
-        self.qTimer.setInterval(3000)
+        self.qTimer.setInterval(INTERVAL)
 
         self.p = photobooth()
+        self.p.Collect_Existing_Images()
+        
+        #self.p.GetNewImages()
 
-        self.counter = 0
+        #self.counter = 0
         #self.images = ["C:\\Users\\ebennett2\\PycharmProjects\\Analysis\\venv\\pgat\\ghd_icon.jpg",
         #          "C:\\Users\\ebennett2\\PycharmProjects\\Analysis\\venv\\pgat\\transco_icon.jpg"]
-        self.images = ['./static/images\P1060868.JPG', './static/images\P1060869.JPG','./static/images\P1060870.JPG', './static/images\P1060871.JPG']
+        #self.images = ['./static/images\P1060868.JPG', './static/images\P1060869.JPG','./static/images\P1060870.JPG', './static/images\P1060871.JPG']
         self.qTimer.timeout.connect(lambda: self.update_image())
         self.qTimer.start()
 
@@ -94,10 +93,27 @@ class App(QWidget):
 
     def update_image(self):
         # self.p.PrintListPhotos()
-        image = int(fmod(self.counter, 2))
-        pixmap = QPixmap(self.images[image])
+        #image = int(fmod(self.counter, 2))
+        self.p.GetNewImages(CAM, DIR)
+        self.p.Collect_Existing_Images()
+        # Let's sort the images by view count to see the next one to display. 
+        # Potentially need a weighting to stop the same image appearing for too many consectutive runs. 
+        # A comparison with the sorted 1 element will tell me how far away it is. 
+        self.p.SortCatalog()
+        image = self.p.catalog[0]
+        print("Lowest after sort:" , self.p.catalog[0].name , " " , self.p.catalog[0].view_count)
+        print("Second Lowest after sort:" , self.p.catalog[1].name , " " , self.p.catalog[1].view_count)
+        lowest_vc=self.p.catalog[0].view_count
+        second_lowest_vc=self.p.catalog[1].view_count
+        if (( second_lowest_vc - lowest_vc) >= 3):
+            #This operates as a bit of a rubberband to avoid new images staying on screen. 
+            # suggest we use a recent images list to repick if the same images are appearing.
+            self.p.catalog[0].view_count=self.p.catalog[1].view_count - 2
+        
+        self.p.catalog[0].incrementViewCount()
+        pixmap = QPixmap(image.path)
         self.window.setPixmap(pixmap.scaledToHeight(self.height))
-        self.counter = self.counter + 1
+        #self.counter = self.counter + 1
 
 
 class photobooth:
@@ -112,76 +128,62 @@ class photobooth:
     # There will be some existing images which we'll need to load in first.
     # This will also allow the restart of the application without losing images.
     def Collect_Existing_Images(self):
-        for img in glob.glob('./static/images/*.jpg'):
+        for img in glob.glob(DIR +'*.jpg'):
 
-            if img not in self.catalog.keys():
-                self.addToCatalog(image(img.split('\\')[1], img, ))
+            if(any(x.path == img for x in self.catalog )):
+                pass
+                #print("Got it mate", img)
             else:
-                print("Got it mate", img)
+                self.addToCatalog(image(img.split('\\')[1], img, ))
         return True
 
     def PrintListPhotos(self):
         print("NAME\tPATH\tVIEWCOUNT\n")
-        for img in self.catalog.keys():
-            print(self.catalog[img].getName(), "\t", self.catalog[img].getPath(), "\t",
-                  self.catalog[img].getViewCount())
+        for img in self.catalog:
+            print(self.catalog[img].name, "\t", self.catalog[img].path, "\t",
+                  self.catalog[img].view_count)
         return True
 
     def SortCatalog(self):
-        return min(sorted(self.catalog, key=operator.attrgetter('view_count')))
+        #return min(sorted(self.catalog, key=operator.attrgetter('view_count')))
+        self.catalog.sort(key=lambda x: x.view_count)
 
     # GetNewImages()
     # This method will make a request call to the wifi card and if a new image exists
     # stream the image down.
-    # CHANGES NEEDED TO WORK IN NEW ENVIRONMENT AND TO USE NEW DATA STRUCTURES
     def GetNewImages(self, folder, save_path):
-        r = requests.get('http://ezshare.card/photo?fdir=' + folder, allow_redirects=False)
-        last = ''
-        script = BeautifulSoup(r.text, 'lxml').find_all('title')
-        bt_redirect = False
-        if script:
-            for s in script:
-                app.logger.info("checking Title:%s, type %s", s, type(s))
-                tmp = s.text
-                if "Photo Gallery" not in tmp:
-                    app.logger.info("BT redirect %s", tmp)
-                    bt_redirect = True
-                    break
+        try:
+            r = requests.get('http://ezshare.card/photo?fdir=' + folder, allow_redirects=False)
 
-        else:
-            bt_redirect = False
-            # Was the request successful, continue to start pull the images down.
-            app.logger.info("status: %s, bt redirect %s", r.status_code, bt_redirect)
-        if r.status_code == 200 and bt_redirect == False:
-            app.logger.info("history: %s\n %s\n %s\n %s", r.history, r.headers, r.json, r.text)
+            if r.status_code == 200 :
+                #print("history: %s\n %s\n %s\n %s", r.history, r.headers, r.json, r.text)
 
-            tags = BeautifulSoup(html, 'lxml').find_all('a')
-            app.logger.info("tags: %s", tags)
-            # Loop throught the 'a' tags to identify the download links.
-            for t in tags:
-                link = t.attrs['href']
-                app.logger.info("link: %s", link)
-                if ".JPG" in link:
-                    if "download" in link:
-                        # Once we have them, can we call them and save them.
-                        url = SITE_BASE + link
-                        # split and get the JPG name
-                        path = save_path + url.split("=")[1].split("&")[0]
-                        app.logger.info("PATH: %s", path)
-                        if not os.path.isfile(path):
-                            img = requests.get(url, stream=True)
-                            # If we get a successful response, stream the image to the path.
-                            if img.status_code == 200:
-                                with open(path, 'wb') as f:
-                                    img.raw.decode_content = True
-                                    shutil.copyfileobj(img.raw, f)
-                                    app.logger.info("found new image %s", path)
-                                    last = path
-        else:
-            app.logger.info('looking for last image')
-            last = str(max(glob.glob(save_path + '*.jpg'), key=os.path.getmtime))
-            app.logger.info("no new images, here's the last one %s", last)
-        return last
+                tags = BeautifulSoup(r.text, 'lxml').find_all('a')
+                # Loop throught the 'a' tags to identify the download links.
+                for t in tags:
+                    link = t.attrs['href']
+                    #print("link:", link)
+                    if ".JPG" in link:
+                        if "download" in link:
+                            # Once we have them, can we call them and save them.
+                            url = SITE_BASE + link
+                            # split and get the JPG name
+                            path = save_path + url.split("=")[1].split("&")[0]
+                            #print("PATH:", path)
+                            if not os.path.isfile(path):
+                                img = requests.get(url, stream=True)
+                                # If we get a successful response, stream the image to the path.
+                                if img.status_code == 200:
+                                    with open(path, 'wb') as f:
+                                        img.raw.decode_content = True
+                                        shutil.copyfileobj(img.raw, f)
+                                        print("found new image", path)
+                                        #time.sleep(2)
+            else:
+                pass
+
+        except:
+            pass
 
 
 if __name__ == "__main__":
